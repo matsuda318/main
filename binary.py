d0e3a670c2dd9cc99a124738654ee14e3b76afc1
matsuda318 <92680881+matsuda318@users.noreply.github.com>
2025-06-08 22:00:29 +0900

    Update binary.py

--- binary.py
+++ binary.py
@@ -123,8 +123,8 @@ class binary_regression:
         z = self._get_z_y(x)
         z = z @ self.coef
         func = self._get_func()
-        v = func(z)
-        v = np.concatenate([1-v[0],v[0]], axis=1)
+        v = np.array(func(z)[0]).reshape(-1,1)
+        v = np.concatenate([1-v,v], axis=1)
         v = np.argmax(v, axis=1)
         return accuracy_score(y, v) 
         
